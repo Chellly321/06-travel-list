8b196ba35b0a5bb44d39e22e83b63272fddb8838
Chelly321 <michelle.royer@icloud.com>
2023-10-30 19:53:02 +0100

    controlled elements PT2

--- src/Form.js
+++ src/Form.js
@@ -2,15 +2,28 @@ import { useState } from "react";
 
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    //form doesn't submit when field is empty
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    // set fields back to intial state
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?😍</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
